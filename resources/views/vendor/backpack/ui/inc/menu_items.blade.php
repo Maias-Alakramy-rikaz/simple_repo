46fe8d11f6a47891b506e87ff99ef554213ef994
Maias <maias-alakramy01@hotmail.com>
2024-11-11 23:15:45 +0300

    Change order and icons of panel

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,11 +1,11 @@
 {{-- This file is used for menu items by any Backpack v6 theme --}}
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i>لوحة التحكم</a></li>
 
-<x-backpack::menu-item title="Exports" icon="la la-question" :link="backpack_url('export')" />
-<x-backpack::menu-item title="Exporters" icon="la la-question" :link="backpack_url('exporter')" />
-<x-backpack::menu-item title="Groups" icon="la la-question" :link="backpack_url('group')" />
-<x-backpack::menu-item title="Imports" icon="la la-question" :link="backpack_url('import')" />
-<x-backpack::menu-item title="Importers" icon="la la-question" :link="backpack_url('importer')" />
-<x-backpack::menu-item title="Products" icon="la la-question" :link="backpack_url('product')" />
-<x-backpack::menu-item title="Product images" icon="la la-question" :link="backpack_url('product-image')" />
-<x-backpack::menu-item title="Users" icon="la la-question" :link="backpack_url('user')" />
+<x-backpack::menu-item title="مستخدمين" icon="la la-user" :link="backpack_url('user')" />
+<x-backpack::menu-item title="زبائن" icon="la la-user" :link="backpack_url('importer')" />
+<x-backpack::menu-item title="موردين" icon="la la-user" :link="backpack_url('exporter')" />
+<x-backpack::menu-item title="مجموعات" icon="la la-tag" :link="backpack_url('group')" />
+<x-backpack::menu-item title="منتجات" icon="la la-tag" :link="backpack_url('product')" />
+<x-backpack::menu-item title="صادرات" icon="la la-tag" :link="backpack_url('import')" />
+<x-backpack::menu-item title="واردات" icon="la la-tag" :link="backpack_url('export')" />
+{{-- <x-backpack::menu-item title=" ProductImages" icon="la la-tag" :link="backpack_url('product-image')" /> #TODO --}}
