8407b42462e02066057b315569eb78351995eaac
Maias <maias-alakramy01@hotmail.com>
2024-11-12 00:03:35 +0300

    Email should be unique

--- database/migrations/2024_11_11_132806_create_exporters_table.php
+++ database/migrations/2024_11_11_132806_create_exporters_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('phone_number', 15)->unique();
-            $table->string('email');
+            $table->string('email')->unique();
             $table->timestamps();
         });
 
