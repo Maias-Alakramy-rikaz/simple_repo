03a50a7a550faf252e31629c20c9438d26372d4d
Maias <maias-alakramy01@hotmail.com>
2024-11-12 13:43:21 +0300

    Yet another translation mistake

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -2,8 +2,8 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i>لوحة التحكم</a></li>
 
 <x-backpack::menu-item title="مستخدمين" icon="la la-user" :link="backpack_url('user')" />
-<x-backpack::menu-item title="زبائن" icon="la la-user" :link="backpack_url('importer')" />
-<x-backpack::menu-item title="موردين" icon="la la-user" :link="backpack_url('exporter')" />
+<x-backpack::menu-item title="موردين" icon="la la-user" :link="backpack_url('importer')" />
+<x-backpack::menu-item title="زبائن" icon="la la-user" :link="backpack_url('exporter')" />
 <x-backpack::menu-item title="مجموعات" icon="la la-tag" :link="backpack_url('group')" />
 <x-backpack::menu-item title="منتجات" icon="la la-tag" :link="backpack_url('product')" />
 <x-backpack::menu-item title="واردات" icon="la la-tag" :link="backpack_url('import')" />
